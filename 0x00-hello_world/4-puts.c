bad36ca9972f031717ed647ca6401739c72a018d
nickson-majawa <nickson.majawa@gmail.com>
2021-06-19 11:49:32 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ *main - prints value using the puts function
+ *return - exits the program
+ */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
