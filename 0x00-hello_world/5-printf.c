eec3edb8e8f57c1eb465a4c6c98bf34322b9bd9b
nickson-majawa <nickson.majawa@gmail.com>
2021-06-20 13:27:08 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  *main - prints value with the printf function
- *return - exits the program
+ *Return: exits the program
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
