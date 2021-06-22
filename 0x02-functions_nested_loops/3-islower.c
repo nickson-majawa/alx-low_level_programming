fe0b6bd88c2de7c3ab42fc9abecd4394a1d35004
nickson-majawa <nickson.majawa@gmail.com>
2021-06-22 13:54:32 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ *_islower = checks for lowercase alphabet letters
+ * Return: returns the lowercase letters
+ */
+int _islower(int c)
+{
+return ( c >= 'a' && c <= 'z');
+}
