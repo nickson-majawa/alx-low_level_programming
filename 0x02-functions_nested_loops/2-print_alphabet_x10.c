90efca438cdd765aeabc972e082d60809ac5f069
nickson-majawa <nickson.majawa@gmail.com>
2021-06-22 13:39:03 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ * Ends with a new line
+ */
+void print_alphabet_x10(void)
+{
+char alp;
+int i;
+for (i = 0; i <=9; i++)
+{
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+_putchar(alp);
+}
+_putchar('\n');
+}
+}
