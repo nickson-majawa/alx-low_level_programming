e57d59c9899e8957eff5936e48a83f18e6f8f0e2
nickson-majawa <nickson.majawa@gmail.com>
2021-06-23 06:40:12 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - finds the absolute value of a given integer
+ * @n: the int value to check
+ * Return: returns the absolute value
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
