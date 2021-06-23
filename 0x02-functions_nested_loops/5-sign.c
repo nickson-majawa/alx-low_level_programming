243ba5b0f996bae9e7f90e0986ff85127efd4b35
nickson-majawa <nickson.majawa@gmail.com>
2021-06-23 05:55:33 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the ASCII value of the number
+ * @n: the int value to check
+ * Return: returns 1 and prints + if n is greater than zero
+ * returns 0 and prints 0 if n is zero
+ * returns -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+  if (n > 0)
+    {
+      _putchar('+');
+      return (1);
+    }
+  else if (n == 0)
+    {
+      _putchar(48);
+      return (0);
+    }
+  else if (n < 0)
+    {
+      _putchar('-');
+    }
+  return (-1);
+}
