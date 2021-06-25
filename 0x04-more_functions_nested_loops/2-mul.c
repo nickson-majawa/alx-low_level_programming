d1a04ceca639b955d32e779de7461d59488d1f01
nickson-majawa <nickson.majawa@gmail.com>
2021-06-25 08:02:35 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - multiplies 2 integers a and b
+ * @a: paramater value used for multiplication
+ * @b: other parameter value used for multiplication
+ * Return: return multiplication result
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
