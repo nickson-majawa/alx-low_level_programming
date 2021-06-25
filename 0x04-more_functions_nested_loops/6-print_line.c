fbba45696c074ce45fa56b955e2159341b845fa6
nickson-majawa <nickson.majawa@gmail.com>
2021-06-25 09:56:43 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_line - print lines
+ * @n: number of lines
+ * Return: the result of the operation
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
