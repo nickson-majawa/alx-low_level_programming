04119a28c95119c8eb11b01d17c440eaee8a2f9b
nickson-majawa <nickson.majawa@gmail.com>
2021-06-25 09:39:39 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * more_numbers -multiplies two integers
+ * Return: the result of the operation
+ */
+void more_numbers(void)
+{
+int i, j = 0;
+while (j < 10)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i >= 10)
+{
+_putchar(i / 10 + '0');
+}
+_putchar(i % 10 + '0');
+}
+j++;
+_putchar('\n');
+}
+}
