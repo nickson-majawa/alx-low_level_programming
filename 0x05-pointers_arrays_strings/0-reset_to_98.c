d975f1147b22085e3f2b71e25eb1de077cc7f3ac
nickson-majawa <nickson.majawa@gmail.com>
2021-06-28 09:14:29 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * reset_to_98 - updates the value it points to 98
- * @*n: the pointer variable name
+ * @n: the pointer variable name
  * Return: returns the pointer value
  */
 void reset_to_98(int *n)
