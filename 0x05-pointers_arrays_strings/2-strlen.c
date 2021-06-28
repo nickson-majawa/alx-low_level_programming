7374f6cb780311b5ca4c6b1aea18ff874b993cf4
nickson-majawa <nickson.majawa@gmail.com>
2021-06-28 10:30:14 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the character to measure the length on
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+*s = "Holberton";
+len = strlen(s);
+return (len);
+}
