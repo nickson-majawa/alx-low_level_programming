fffcd33911603c90f3f13c96ddde0d08dcd83826
nickson-majawa <nickson.majawa@gmail.com>
2021-06-28 14:02:51 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copies the sting pointed to by the source
+ * @dest: char type string
+ * @src: char type
+ * Return: void
+ */
+char *_strcpy(char *des, char *src)
+{
+int i = -1;
+do
+{
+i++;
+dest[i] = src[i];
+}
+while (src[i] != '\0');
+return (dest);
+}
