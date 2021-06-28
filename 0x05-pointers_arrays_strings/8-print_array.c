d65b1cd2f4b78f8925ef265c09c9bcc32ba703e1
nickson-majawa <nickson.majawa@gmail.com>
2021-06-28 13:52:14 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * @n: number of elements printed
+ * @a: array pointer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+i = 0;
+for (n--; n >=0; n--, i++)
+{
+printf("%d", a[i]);
+if (n > 0)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
