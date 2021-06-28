55faa0a77e3867aa8036b5a0442fc4c92cdc8739
nickson-majawa <nickson.majawa@gmail.com>
2021-06-28 12:46:35 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+void rev_string(cha *s)
+{
+int i, len;
+i = 0;
+len = 0;
+while(*(s + i))
+{
+len++;
+i++
+}
+for (i = 0; i < len /2; i++)
+{
+char c = *(s +i);
+*(s+i) = *(s + len - i - 1);
+*(s + len - i - 1) = c;
+}
+}
