2aa44960e70b06d8be5bad0d3a381c248d477a3c
nickson-majawa <nickson.majawa@gmail.com>
2021-07-01 01:14:54 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string.
+ * @dest: pointer parameter destination
+ * @src: pointer parameter source
+ * @n: integer parameter
+ * Return: dest variable
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, k = 0;
+while (src[k])
+{
+k++;
+}
+if (n < k)
+{
+for (i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+if (i < k)
+{
+dest[i] = src[i];
+}
+else
+{
+dest[i] = '\0';
+}
+}
+}
+return (dest);
+}
