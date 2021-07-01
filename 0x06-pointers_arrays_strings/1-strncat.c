03a1400fe7ac0fab554eea07e9e3cd1e037b1036
nickson-majawa <nickson.majawa@gmail.com>
2021-07-01 00:09:48 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *_strncat - concatenates two strings
+ * @dest: pointer parameter destination
+ * @src: pointer parameter source
+ * @n: parameter integer number
+ * Return: returns dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0, x = 0, k = 0, y = 0;
+while (dest[i])
+{
+i++;
+}
+while (src[y])
+{
+y++;
+}
+if (n < y)
+{
+n += i;
+for (j = i; j < n; j++)
+{
+dest[j] = src[x];
+x++;
+}
+}
+else
+{
+n = i + y;
+for (k = i; k < n; k++)
+{
+dest[k] = src[x];
+x++;
+}
+}
+return (dest);
+}
