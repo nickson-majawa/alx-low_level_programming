2b14943ea1ae3bfdd03d53538b52a361e80240ee
nickson-majawa <nickson.majawa@gmail.com>
2021-07-01 04:20:19 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes lowercase letters to upperscase.
+ * @s: pointer parameter character
+ * Return: s variable pointer to uppercase
+ */
+char *string_toupper(char *s)
+{
+int i;
+for (i = 0; s[i]; i++)
+{
+if (s[i] >= 97 && s[i] <== 122)
+s[i] -= 32;
+}
+return (s);
+}
