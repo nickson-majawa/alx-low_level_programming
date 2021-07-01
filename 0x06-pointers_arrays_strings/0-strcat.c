581670a1a514e04df18636ba6d42428b96fa318c
nickson-majawa <nickson.majawa@gmail.com>
2021-06-30 23:30:44 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int i, srcLength, destLength;
-i = 0, destLenth = 0, srcLength = 0;
-while (*(dest + i))
+int i, j, y, x;
+i = 0, j = 0, y = 0, x = 0;
+while (src[i])
 {
-destLength++;
 i++;
 }
-i = 0;
-while (*(src + i))
+while (dest[j])
 {
-srcLength++;
-i++;
+j++;
 }
-for (i = 0; i < srcLength; i++)
+i += j;
+for (y = j; y < i; y++)
 {
-*(dest + desLength + i) = *(src + i);
+dest[y] = src[x];
+x++;
 }
 return (dest);
 }
