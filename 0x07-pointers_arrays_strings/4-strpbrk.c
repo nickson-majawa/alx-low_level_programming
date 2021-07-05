0bea949683c6072989b13e81e38e44446dcf8b01
nickson-majawa <nickson.majawa@gmail.com>
2021-07-05 09:43:38 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: pointer parameter
+ * @accept: pointer parameter
+ * Return: s variable
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j, n = 0, x = 0;
+while (s[n])
+n++;
+while (accept[x])
+x++;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= x; j++)
+{
+if (s[i] == accept[j])
+return (s + i);
+}
+}
+s = 0;
+return (s);
+}
