1b27847a238b68b611351a3f9f542b914455b225
nickson-majawa <nickson.majawa@gmail.com>
2021-07-05 04:14:40 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer parameter destination
+ * @src: pointer parameter source
+ * @n: size
+ * Return: dest variable
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(dest + i) = src[i];
+}
+return (dest);
+}
