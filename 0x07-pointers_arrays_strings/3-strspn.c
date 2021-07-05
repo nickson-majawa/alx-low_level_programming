7a02355e4495ab896a79cee50be598d17bd25b3e
nickson-majawa <nickson.majawa@gmail.com>
2021-07-05 04:54:33 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the lenth of a prefix substring.
+ * @s: pointer parameter
+ * @accept: pointer parameter, value accept
+ * Return: counter
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j, n = 0, x = 0;
+unsigned int count = 0;
+while (s[x])
+x++;
+while (accept[n])
+n++;
+for (i = 0; i < x; i++)
+{
+for (j = 0; j <= n; j++)
+{
+if (accept[j] == s [i])
+{
+count++;
+break;
+}
+if (!accept[j])
+i = x;
+}
+}
+return (count);
+}
