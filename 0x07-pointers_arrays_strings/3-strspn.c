c60f68b4a8c732e17351d933401a37621b694350
nickson-majawa <nickson.majawa@gmail.com>
2021-07-05 04:59:05 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,14 +3,16 @@
  * _strspn - gets the lenth of a prefix substring.
  * @s: pointer parameter
  * @accept: pointer parameter, value accept
- * Return: counter
+ * Return: count
  */
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j, n = 0, x = 0;
 unsigned int count = 0;
 while (s[x])
+{
 x++;
+}
 while (accept[n])
 n++;
 for (i = 0; i < x; i++)
