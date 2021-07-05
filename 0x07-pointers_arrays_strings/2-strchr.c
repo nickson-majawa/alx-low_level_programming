8423f096dacd2ad7cb6683120a83a2b439134eab
nickson-majawa <nickson.majawa@gmail.com>
2021-07-05 04:32:24 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strchr - Function returns first occurence of a character
+ * @s: String to be examined
+ * @c: Character to be located
+ * Return: returns @s
+ */
+char *_strchr(char *s, char c)
+{
+do {
+if (*s == c)
+break;
+}
+while (*s++);
+return (s);
+}
