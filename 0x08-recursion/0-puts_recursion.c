456ce9643944069536b60a846d52ba924e50c0dd
nickson-majawa <nickson.majawa@gmail.com>
2021-07-07 03:08:27 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: pointer parameter char
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(*s);
+++s;
+_puts_recursion(s);
+}
+else
+_putchar('\n');
+}
