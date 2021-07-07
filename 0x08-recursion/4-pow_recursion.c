de952ccf5ffe24f577aa3f56911b1a1a68e005a8
nickson-majawa <nickson.majawa@gmail.com>
2021-07-07 09:33:45 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - returns the value of x raised to the opwer of y
+ * @x: pointer parameter integer
+ * @y: pointer parameter integer
+ * Return: pow number
+ */
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+return (_pow_recursion(x, y - 1) * x);
+else if (y == 0)
+return (1);
+else
+return (-1);
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -4,3 +4,4 @@ char _putchar(char s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
