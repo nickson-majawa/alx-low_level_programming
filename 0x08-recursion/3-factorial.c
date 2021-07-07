a2f9ee1609c06de5c0b3c4d38171bb7870426d64
nickson-majawa <nickson.majawa@gmail.com>
2021-07-07 08:54:51 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * factorial -calculates factorial of given number
+ * @n -the given number parameter
+ * Return: factorial
+ */
+int factorial(int n)
+{
+if (n > 0)
+{
+return (n * factorial(n - 1));
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+return (-1);
+}
