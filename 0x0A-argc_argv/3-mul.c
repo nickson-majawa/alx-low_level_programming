ba73d7a30739ad029629d61a2ac698b3fd2a5bc9
nickson-majawa <nickson.majawa@gmail.com>
2021-07-12 10:05:30 -0700

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints multiplication result
+ * @argc: size parameters
+ * @argv: array pointer parameters
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+int i, j;
+if (argc > 2)
+{
+i = atoi(argv[1]);
+j = atoi(argv[2]);
+printf("%d\n", i * j);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
