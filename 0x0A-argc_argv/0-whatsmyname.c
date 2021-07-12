23c05f45ea521d4961ecb96f881769d173f8d024
nickson-majawa <nickson.majawa@gmail.com>
2021-07-12 09:30:44 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: size parameters
+ * @argv: array pointer parameters
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+if (argc >= 0)
+printf("%s\n", argv[0]);
+return (0);
+}
