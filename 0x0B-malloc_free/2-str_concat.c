1a0e0761b2881a302a3682607ec3c5c2846666ac
nickson-majawa <nickson.majawa@gmail.com>
2021-07-13 09:11:23 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,23 +11,18 @@ char *str_concat(char *s1, char *s2)
 char *ptr;
 int len = 0, len1 = 0, i = 0, j = 0, k = 0;
 if (s1 == 0)
-s1 = "";
- 
+s1 = ""; 
 if (s2 == 0)
 s2 = "";
- 
 while (s1[len1])
 len++;
- 
 while (s2[len])
 len++;
- 
 ptr = malloc(sizeof(char) * (len + len) + 1);
 if (!(ptr == NULL))
 {
 for (i = 0; i < len; i++)
 ptr[i] = s1[i];
-
 for (j = i; j <= (len + len1); j++)
 {
 ptr[j] = s2[k];
