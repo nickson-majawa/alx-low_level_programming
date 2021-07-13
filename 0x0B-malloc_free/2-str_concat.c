a7e468bc30d79c02d0f410c125824e870cc1219c
nickson-majawa <nickson.majawa@gmail.com>
2021-07-13 08:30:01 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _str_concat - concats two strings
+ * @s1: pointer character
+ * @s2: another pointer character
+ * Return: pointe value
+ */
+char *_str_concat(char *s1, char *s2)
+{
+char *ptr;
+int len = 0, len1 = 0, i = 0, j = 0, k = 0;
+if (s1 == 0)
+s1 = "";
+ 
+if (s2 == 0)
+s2 = "";
+ 
+while (s1[len1])
+len++;
+while (s2[len2])
+len++;
+ 
+ptr = malloc((sizeof(char) *(len + len) +1);
+if (!(ptr == NULL))
+{
+for (i = 0; i < len; i++)
+ptr[i] = s1[i];
+
+for (j = i; j <= (len + len1); j++)
+{
+ptr[j] = s2[k];
+k++;
+}
+return (ptr);
+}
+else
+return (NULL);
+}
