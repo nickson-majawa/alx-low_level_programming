dab5deadeea36905bdd816a9f10b7a0fb7b89a8b
nickson-majawa <nickson.majawa@gmail.com>
2021-07-13 09:23:06 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ while (s1[len1])
 len++;
 while (s2[len])
 len++;
-ptr = malloc(sizeof(char) * (len + len) + 1);
+ptr = malloc(sizeof(char) * (len + len1) + 1);
 if (!(ptr == NULL))
 {
 for (i = 0; i < len; i++)
