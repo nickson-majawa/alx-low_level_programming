ad13718bb21070f0bcb8c2237f0fb0fc93ff305d
nickson-majawa <nickson.majawa@gmail.com>
2021-07-13 07:53:30 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - returns pointer to new space in memory
+ * @str: pointer character
+ * Return: pointe value
+ */
+char *_strdup(char *str)
+{
+char *ptr;
+ int len = 0, i = 0;
+ if (str == NULL)
+   return (NULL);
+ while (str[len])
+   len++;
+ ptr = malloc((sizeof(*ptr) * len) +1);
+ if (!(ptr == NULL))
+   {
+     for (i = 0; i < len; i++)
+       ptr[i] = str[i];
+     return (ptr);
+   }
+ else
+   return (NULL);
+}
