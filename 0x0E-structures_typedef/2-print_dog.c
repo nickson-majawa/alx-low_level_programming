57967e4b7083cf649181a98b80f871fb72800570
nickson-majawa <nickson.majawa@gmail.com>
2021-07-19 15:25:32 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 if (d)
 {
 if (!d->name)
-d->name = "(nill)";
+d->name = "(nil)";
 if (!d->owner)
-d->owner = "(nill)";
+d->owner = "(nil)";
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
 }
