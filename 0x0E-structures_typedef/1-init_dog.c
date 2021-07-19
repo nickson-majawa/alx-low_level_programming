8925bf8b6aaa97bef547848aba83076a8cbe2e05
nickson-majawa <nickson.majawa@gmail.com>
2021-07-19 13:54:02 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize a variable of type strct dog
+ * @d: dog structure
+ * @name: name init
+ * @age: age init
+ * @owner: owner init
+ * Return: 0 success
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,9 +9,13 @@
  */
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
