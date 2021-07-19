2a1a560398b729607a37944fd0da706e0a8b39fd
nickson-majawa <nickson.majawa@gmail.com>
2021-07-19 13:39:18 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - varibles for dog
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
