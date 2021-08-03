41c7aa7075e2536fb62293cff08b2159411e10a3
nickson-majawa <nickson.majawa@gmail.com>
2021-08-03 11:21:25 -0700

    Task 8

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
