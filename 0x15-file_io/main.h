0606da7e618d6d582ab2a89afd47d2344563e89f
nickson-majawa <nickson.majawa@gmail.com>
2021-08-09 12:06:32 -0700

    file io project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_FH
 #define MAIN_FH
+
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 ssize_t read_textfile(const char *filename,size_t letters);
